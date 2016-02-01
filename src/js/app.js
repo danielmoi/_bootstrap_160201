114b32640de6590d9a861158b28ba4c17318cdd0
danielmoi <daniel@ampersandmoi.com>
2016-02-02 09:40:46 +1100

    Main sections completed

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+(function () {
+  var start = 'daniel',
+    at = '&#64;',
+    middle = 'ampersandmoi',
+    dot = '&#46;',
+    finish = 'com';
+  $('#email').html(start + at + middle + dot + finish);
+}());
